36d429219d410f4da4e201ddaa729bcac93e0ea0
Pavan Subhash Chandrabose Nara <npsc1601@gmail.com>
2026-03-25 12:12:27 -0700

    Add requirements.txt

--- feedback-form/app.py
+++ feedback-form/app.py
@@ -60,5 +60,5 @@ def submit_feedback():
 
     return jsonify({"status": "success", "message": "Feedback submitted successfully"}), 200
 
-    if __name__ == "__main__":
-        app.run(host="0.0.0.0", port=8080)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
